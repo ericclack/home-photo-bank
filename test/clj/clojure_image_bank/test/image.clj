d3d6f74d056b918af20aa0c511eb246ec6008abe
Eric Clack <eric@bright-interactive.com>
2017-02-28 08:46:32 +0000

    More testing

--- test/clj/clojure_image_bank/test/image.clj
+++ test/clj/clojure_image_bank/test/image.clj
@@ -3,17 +3,19 @@
             [ring.mock.request :refer :all]
             [clojure-image-bank.handler :refer :all]
             [clojure.java.io    :as io]
+            [clojure.string :as s]
             [environ.core :refer [env]]
             [image-resizer.core :refer :all]
             [image-resizer.format :as format]))
 
-(defn media-path [file]
-  (str (env :media-path) "/" file))
+(defn media-path
+  [& path]
+   (.getPath (apply io/file (cons (env :media-path) path))))
 
 (deftest test-image
   (testing "resize"
-    (let [file (io/file (media-path "2017/2/boat.jpg"))
+    (let [file (io/file (media-path "2017" "2" "boat.jpg"))
           thumbnail (resize file 100 100)
           outfilepath (format/as-file thumbnail (media-path "out.jpg"))]
-      (is (= (media-path "out_50x33.jpg") outfilepath)))))
+      (is (s/ends-with? outfilepath (media-path "out_100x100.jpg"))))))
 

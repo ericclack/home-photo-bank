c8f53a90d53558592996a215c517609405f3373a
Eric Clack <eric@bright-interactive.com>
2017-03-06 14:59:56 +0000

    Refactoring into image-store + methods for import

---
+++ src/clj/clojure_image_bank/image_store.clj
@@ -0,0 +1,73 @@
+(ns clojure-image-bank.image-store
+  (:require [environ.core :refer [env]]
+            [clojure.java.io    :as io]
+            [clojure.string :as s]
+            ;; -------
+            [image-resizer.core :refer :all]
+            [image-resizer.format :as format]
+            [clj-exif.core :as exif]
+            [clj-time.core :as t]
+            [clj-time.format :as tf]))
+
+;; -------------------------------------------------------
+
+(defn media-path
+  "A file object for this path inside the media directory.
+  Path is expressed as a list of directories and an optional file"
+  [& path]
+  (apply io/file (cons (env :media-path) path)))
+
+(defn media-path-string
+  "As media-path, but as a string"
+  [& path]
+  (.getPath (apply media-path path)))
+
+;; -------------------------------------------------------
+
+(defn get-exif-metadata
+  [file]
+  (let [metadata (exif/get-metadata file)]
+    (if metadata (exif/read metadata))))
+
+(def exif-date-format "yyyy:MM:dd HH:mm:ss")
+
+(defn get-date-time
+  "EXIF time is in format: 2003:12:14 12:01:44"
+  [metadata]
+  (tf/parse (tf/formatter exif-date-format)
+            (get-in metadata ["Root" "DateTime"])))
+
+(defn get-orientation
+  [metadata]
+  (get-in metadata ["Root" "Orientation"]))
+
+;; -------------------------------------------------------
+
+(defn images-to-import
+  "JPG image files in the media/import directory"
+  []
+  (filter #(s/ends-with? (s/lower-case %) ".jpg")
+          (file-seq (media-path "import"))))
+
+(defn media-path-for-image
+  "Image path is Year/Month/Day/Filename based on DateTime in EXIF data"
+  [image-file]
+  (let [d (get-date-time (get-exif-metadata image-file))]
+    (media-path (str (t/year d))
+                (str (t/month d))
+                (str (t/day d))
+                (.getName image-file))))
+
+(defn move-image-into-store
+  "Move image from import into store"
+  [image-file]
+  (let [destination (media-path-for-image image-file)]
+    (.mkdirs (.getParentFile destination))
+    (.renameTo image-file destination)))
+
+(defn import-images
+  "Process images and store them away.
+  TODO: what about images that have no EXIF data?"
+  []
+  (map #(move-image-into-store %)
+       (images-to-import)))
--- test/clj/clojure_image_bank/test/image.clj
+++ test/clj/clojure_image_bank/test/image.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [ring.mock.request :refer :all]
             [clojure-image-bank.handler :refer :all]
+            [clojure-image-bank.image-store :as imgs]
+            ;;---
             [clojure.java.io    :as io]
             [clojure.string :as s]
             [environ.core :refer [env]]
@@ -11,43 +13,19 @@
             [clj-time.core :as t]
             [clj-time.format :as tf]))
 
-(defn media-file
-  [& path]
-  (apply io/file (cons (env :media-path) path)))
-
-(defn media-path
-  [& path]
-  (.getPath (apply media-file path)))
-
-(defn get-exif-metadata
-  [file]
-  (let [metadata (exif/get-metadata file)]
-    (if metadata (exif/read metadata))))
-
-(def exif-date-format "yyyy:MM:dd HH:mm:ss")
-
-(defn get-date-time
-  "EXIF time is in format: 2003:12:14 12:01:44"
-  [metadata]
-  (tf/parse (tf/formatter exif-date-format)
-            (get-in metadata ["Root" "DateTime"])))
-
-(defn get-orientation
-  [metadata]
-  (get-in metadata ["Root" "Orientation"]))
 
 ;; -----------------------------------------------------
 
 (deftest test-image
   (testing "resize"
-    (let [file (media-file "test" "boat.jpg")
+    (let [file (imgs/media-path "test" "boat.jpg")
           thumbnail (resize file 100 100)
-          outfilepath (format/as-file thumbnail (media-path "out.jpg"))]
-      (is (s/ends-with? outfilepath (media-path "out_100x100.jpg"))))))
+          outfilepath (format/as-file thumbnail (imgs/media-path-string "out.jpg"))]
+      (is (s/ends-with? outfilepath (imgs/media-path-string "out_100x100.jpg"))))))
 
 (deftest test-exif
   (testing "Exif data from images"
-    (let [file (media-file "test" "flower_exiv2.jpg")
-          metadata (get-exif-metadata file)]
-      (is (= 1 (get-orientation metadata)))
-      (is (= 2003 (t/year (get-date-time metadata)))))))
+    (let [file (imgs/media-path "test" "flower_exiv2.jpg")
+          metadata (imgs/get-exif-metadata file)]
+      (is (= 1 (imgs/get-orientation metadata)))
+      (is (= 2003 (t/year (imgs/get-date-time metadata)))))))

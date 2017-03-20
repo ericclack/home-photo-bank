7c0796c9e7491ba2b814ab5de855c69df6df0122
Eric Clack <eric@bright-interactive.com>
2017-03-20 15:47:16 +0000

    set-up code

---
+++ src/clj/clojure_photo_bank/models/setup.clj
@@ -0,0 +1,24 @@
+(ns ^{:doc "Set up CouchDB for the photo bank. Run these functions 
+            once after install with `lein setup-db`"}
+    clojure-photo-bank.models.setup
+  (:require [com.ashafa.clutch :as couch]
+            [clojure-photo-bank.models.db :as db]))
+
+(defn create-photo-views 
+  "Create CouchDB views photos/by_keyword - a simple search index."
+  []
+  (db/with-db
+    (couch/save-view "photos" (couch/view-server-fns
+                               :javascript
+                               {:by_keyword {:map
+"function(doc) {
+  doc.keywords.forEach( function(word) {
+    emit(word) });
+}"
+                                             }}))))
+
+(defn setup-db
+  "Create all the views we need."
+  []
+  (create-photo-views))
+

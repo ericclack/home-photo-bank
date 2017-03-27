8a311f6319dfa9542f891d4c4b5e3df2e0ac650c
Eric Clack <eric@bright-interactive.com>
2017-03-27 09:46:05 +0100

    photos by category

--- src/clj/clojure_photo_bank/models/setup.clj
+++ src/clj/clojure_photo_bank/models/setup.clj
@@ -10,18 +10,28 @@
   (db/with-db
     (couch/save-view "photos" (couch/view-server-fns
                                :javascript
-                               {:by_keyword
-                                {:map
-"function(doc) {
+{:by_keyword
+ {:map
+  "
+function(doc) {
   doc.keywords.forEach( function(word) {
     if (word.length > 2) {
       emit(word, 1);
     }});
 }"
-                                 :reduce
-"function(key, values, rereduce) {
+  :reduce
+  "
+function(key, values, rereduce) {
   return sum(values);
-}"}}))))
+}"}
+
+ :by_category
+ {:map
+  "
+function(doc) {
+  emit(doc.category, doc);
+}"}}
+))))
 
 (defn setup-db
   "Create all the views we need."

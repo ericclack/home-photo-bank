e4753d4e7a05146e7fb2b4fdd5d7c38efb3640d0
Eric Clack <eric@bn7.net>
2024-12-30 19:45:22 +0000

    Year / month searching

--- resources/tools/photo-metadata-search.py
+++ resources/tools/photo-metadata-search.py
@@ -3,11 +3,12 @@
 import os
 import json
 import fileinput
+import re
 from pprint import pprint
 
-MAX_PHOTOS = 5
-counter = 0
+MAX_PHOTOS = 25
 keyword_to_photos = {}
+yearmonth_to_photos = {}
 
 def open_photos_that_exist(photos):
     # This probably only works on a Mac
@@ -19,18 +20,30 @@ def open_photos_that_exist(photos):
     else:
         print("No files exist")
 
-for line in fileinput.input():
-    counter += 1
-    photo = json.loads(line)
-    for k in photo['keywords']:
-        if k not in keyword_to_photos:
-            keyword_to_photos[k] = []
-        keyword_to_photos[k].append(photo)
+def load_json():
+    for line in fileinput.input():
+        photo = json.loads(line)
+        for k in photo['keywords']:
+            if k not in keyword_to_photos:
+                keyword_to_photos[k] = []
+            keyword_to_photos[k].append(photo)
+
+        r = re.compile('/\d+$') #Match day number incl final /
+        ym = r.sub('', photo['category'])
+        if ym not in yearmonth_to_photos:
+            yearmonth_to_photos[ym] = []
+        yearmonth_to_photos[ym].append(photo)
         
+load_json()
+            
 while True:
-    k = input("Enter single keyword to search: " )
+    k = input("Enter single keyword to search or year/month: " )
     try:
-        photos = keyword_to_photos[k]
+        if re.match('\d\d\d\d/\d+', k):
+            photos = yearmonth_to_photos[k]
+        else: 
+            photos = keyword_to_photos[k]
+
         print("%s results:" % len(photos))
         #pprint(photos)
         

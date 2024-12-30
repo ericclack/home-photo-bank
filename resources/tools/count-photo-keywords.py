4762979f30713ec762ffc832c7b600c118749480
Eric Clack <eric@bn7.net>
2024-12-30 15:20:24 +0000

    Newline isn't a keyword

--- resources/tools/count-photo-keywords.py
+++ resources/tools/count-photo-keywords.py
@@ -7,7 +7,9 @@ remove_path = r"^media/\d+/\d+/\d+/(.*)\d+.jpe?g"
 keywords = {}
 
 for line in fileinput.input():
-    image_keywords = re.sub(remove_path, r"\1", line ).split("-")
+    image_keywords = [k for k
+                      in re.sub(remove_path, r"\1", line ).split("-")
+                      if not(k.isspace())]
     for k in image_keywords:
         keywords[k] = keywords.get(k, 0) + 1
 

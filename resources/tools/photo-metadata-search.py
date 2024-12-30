a61aab4af4ed246b4052a0107987babc927bf273
Eric Clack <eric@bn7.net>
2024-12-30 16:39:31 +0000

    Added

---
+++ resources/tools/photo-metadata-search.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+import json
+import fileinput
+from pprint import pprint
+
+MAX_PHOTOS = 5
+counter = 0
+keyword_to_photos = {}
+
+for line in fileinput.input():
+    counter += 1
+    photo = json.loads(line)
+    for k in photo['keywords']:
+        if k not in keyword_to_photos:
+            keyword_to_photos[k] = []
+        keyword_to_photos[k].append(photo)
+        
+while True:
+    k = input("Enter single keyword to search: " )
+    photos = keyword_to_photos[k]
+    print("%s results:" % len(photos))
+    pprint(photos)
+
+    # This probably only works on a Mac
+    files = " ".join([p['path'] for p in photos])
+    os.system("open %s" % files)
+           
+    
